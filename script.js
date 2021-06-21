9dbb576325a6f3c4347e10b7126519b002b7be80
Hovo Harutyunyan <HovHyan16@gmail.com>
2021-06-21 23:23:47 +0800

    code

---
+++ script.js
@@ -0,0 +1,20 @@
+// const tiv = [3, 4, 5, 6];
+// let result = [];
+// let ara = 0;
+
+// tiv.forEach((element,index) => {
+//     const b = (element * index);
+//     console.log(b);
+//     result.push(b);
+// });
+
+// result.forEach((element) => {
+//     ara = ara + element;
+//     console.log(ara);
+// });
+
+let i = ['H','O','V','O'];
+i.reverse();
+console.log(i);
+i.join('');
+console.log(i.join(''));
